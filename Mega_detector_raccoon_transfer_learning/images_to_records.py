5e5f6f59fd6fdddfbcae65f5c12b8612e6ad0d0c
alnah005 <alnah005@umn.edu>
2021-02-23 18:02:01 -0600

    split training and test data

--- Mega_detector_raccoon_transfer_learning/images_to_records.py
+++ Mega_detector_raccoon_transfer_learning/images_to_records.py
@@ -8,7 +8,7 @@ file: images_to_records.py
 
 @created: 2021-02-22T22:09:11.241Z-06:00
 
-@last-modified: 2021-02-23T01:00:29.954Z-06:00
+@last-modified: 2021-02-23T17:19:57.656Z-06:00
 """
 
 # standard library
@@ -42,7 +42,20 @@ def create_tf_example(image_data):
     width = 1920
     filename = str.encode(image_data['file'])
     image_format = b'jpg'
-    encoded_image = open(os.path.join(b'images',filename), 'rb').read()
+    trainBool = True
+    try:
+        image_file = open(os.path.join(b'images_train',filename), 'rb')
+        encoded_image = image_file.read()
+    except:
+        try:
+            image_file = open(os.path.join(b'images_eval',filename), 'rb')
+            encoded_image = image_file.read()
+            trainBool = False
+        except:
+            print(filename)
+            assert False, "file was not found in either images_train or images_eval"
+
+    image_file.close()
     xmins = [i['bbox'][0] / width for i in detections]
     xmaxs = [i['bbox'][2] / width for i in detections]
     ymins = [i['bbox'][1] / height for i in detections]
@@ -65,19 +78,24 @@ def create_tf_example(image_data):
         'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
         'image/object/class/label': dataset_util.int64_list_feature(classes),
     }))
-    return tf_example
+    return tf_example, trainBool
 
 def main(_):
-    # TODO(user): Write code to read in your dataset to examples variable
     with open('raccoon_volunteer_labels.json') as json_file:
         examples = json.load(json_file)
-    cutoff = 20000
-    for index, example in enumerate(examples['images']):
-        if (index < cutoff):
-            writer = tf.python_io.TFRecordWriter(os.path.join(FLAGS.output_path,'train-{0}-'.format(str(index).zfill(5))+'of-00001.tfrecord'))
+    trainIndex = 0
+    evalIndex = 0
+    for example in examples['images']:
+        try:
+            tf_example, trainBool = create_tf_example(example)
+        except:
+            continue
+        if (trainBool):
+            writer = tf.python_io.TFRecordWriter(os.path.join(FLAGS.output_path,'train-{0}-'.format(str(trainIndex).zfill(5))+'of-00001.tfrecord'))
+            trainIndex += 1
         else:
-            writer = tf.python_io.TFRecordWriter(os.path.join(FLAGS.output_path,'eval-{0}-'.format(str(index - cutoff).zfill(5))+'of-00001.tfrecord'))
-        tf_example = create_tf_example(example)
+            writer = tf.python_io.TFRecordWriter(os.path.join(FLAGS.output_path,'eval-{0}-'.format(str(evalIndex).zfill(5))+'of-00001.tfrecord'))
+            evalIndex += 1
         writer.write(tf_example.SerializeToString())
         writer.close()
 
