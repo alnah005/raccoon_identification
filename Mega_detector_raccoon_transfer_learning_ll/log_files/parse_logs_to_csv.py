02d67eced89d7296286e2b644fef0e9af9240700
alnah005 <alnah005@umn.edu>
2021-03-30 09:09:38 -0500

    added experiment 2 configs

--- Mega_detector_raccoon_transfer_learning_ll/log_files/parse_logs_to_csv.py
+++ Mega_detector_raccoon_transfer_learning_ll/log_files/parse_logs_to_csv.py
@@ -8,7 +8,7 @@ file: parse_logs_to_csv.py
 
 @created: 2021-03-01T13:30:00.015Z-06:00
 
-@last-modified: 2021-03-15T01:22:09.274Z-05:00
+@last-modified: 2021-03-30T09:05:15.436Z-05:00
 """
 
 # standard library
@@ -20,19 +20,20 @@ import numpy as np
 
 # local source
 
-columns = ["Step","DetectionBoxes_Precision/mAP", "DetectionBoxes_Precision/mAP (large)", "DetectionBoxes_Precision/mAP (medium)","DetectionBoxes_Precision/mAP (small)"
-,"DetectionBoxes_Precision/mAP@.50IOU","DetectionBoxes_Precision/mAP@.75IOU","DetectionBoxes_Recall/AR@1","DetectionBoxes_Recall/AR@10","DetectionBoxes_Recall/AR@100",
-"DetectionBoxes_Recall/AR@100 (large)", "DetectionBoxes_Recall/AR@100 (medium)","DetectionBoxes_Recall/AR@100 (small)", "Loss/BoxClassifierLoss/classification_loss",
-"Loss/BoxClassifierLoss/localization_loss", "Loss/RPNLoss/localization_loss","Loss/RPNLoss/objectness_loss","Loss/total_loss","loss"]
-regex = r"INFO:tensorflow:Saving[\s\n]+dict[\s\n]+for[\s\n]+global[\s\n]+step[\s\n]+(\d*?):[\s\n]+DetectionBoxes_Precision\/mAP[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP[\s\n]+\(large\)[\s\n]+=[\s\n]+(.*?), DetectionBoxes_Precision\/mAP[\s\n]+\(medium\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP \(small\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP@\.50IOU[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP@\.75IOU[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@1[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@10[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+\(large\)[\s\n]+=[\s\n]+(.*?), DetectionBoxes_Recall\/AR@100[\s\n]+\(medium\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+\(small\)[\s\n]+=[\s\n]+(.*?), Loss\/BoxClassifierLoss\/classification_loss[\s\n]+=[\s\n]+(.*?), Loss\/BoxClassifierLoss\/localization_loss[\s\n]+=[\s\n]+(.*?), Loss\/RPNLoss\/localization_loss[\s\n]+=[\s\n]+(.*?),[\s\n]+Loss\/RPNLoss\/objectness_loss[\s\n]+=[\s\n]+(.*?),[\s\n]+Loss\/total_loss[\s\n]+=[\s\n]+(.*?), .*?,[\s\n]+loss[\s\n]+=[\s\n]+(.*?)\n"
-outputFile = "performance_result.csv"
+# columns = ["Step","DetectionBoxes_Precision/mAP", "DetectionBoxes_Precision/mAP (large)", "DetectionBoxes_Precision/mAP (medium)","DetectionBoxes_Precision/mAP (small)"
+# ,"DetectionBoxes_Precision/mAP@.50IOU","DetectionBoxes_Precision/mAP@.75IOU","DetectionBoxes_Recall/AR@1","DetectionBoxes_Recall/AR@10","DetectionBoxes_Recall/AR@100",
+# "DetectionBoxes_Recall/AR@100 (large)", "DetectionBoxes_Recall/AR@100 (medium)","DetectionBoxes_Recall/AR@100 (small)", "Loss/BoxClassifierLoss/classification_loss",
+# "Loss/BoxClassifierLoss/localization_loss", "Loss/RPNLoss/localization_loss","Loss/RPNLoss/objectness_loss","Loss/total_loss","loss"]
+# regex = r"INFO:tensorflow:Saving[\s\n]+dict[\s\n]+for[\s\n]+global[\s\n]+step[\s\n]+(\d*?):[\s\n]+DetectionBoxes_Precision\/mAP[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP[\s\n]+\(large\)[\s\n]+=[\s\n]+(.*?), DetectionBoxes_Precision\/mAP[\s\n]+\(medium\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP \(small\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP@\.50IOU[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Precision\/mAP@\.75IOU[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@1[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@10[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+\(large\)[\s\n]+=[\s\n]+(.*?), DetectionBoxes_Recall\/AR@100[\s\n]+\(medium\)[\s\n]+=[\s\n]+(.*?),[\s\n]+DetectionBoxes_Recall\/AR@100[\s\n]+\(small\)[\s\n]+=[\s\n]+(.*?), Loss\/BoxClassifierLoss\/classification_loss[\s\n]+=[\s\n]+(.*?), Loss\/BoxClassifierLoss\/localization_loss[\s\n]+=[\s\n]+(.*?), Loss\/RPNLoss\/localization_loss[\s\n]+=[\s\n]+(.*?),[\s\n]+Loss\/RPNLoss\/objectness_loss[\s\n]+=[\s\n]+(.*?),[\s\n]+Loss\/total_loss[\s\n]+=[\s\n]+(.*?), .*?,[\s\n]+loss[\s\n]+=[\s\n]+(.*?)\n"
+# outputFile = "performance_result.csv"
 
-# columns = ["loss","step"]
-# regex = r"INFO:tensorflow:loss = (.*), step = (.*) \("
-# outputFile = "loss_result.csv"
+columns = ["loss","step"]
+regex = r"INFO:tensorflow:loss = (.*), step = (.*) \("
+outputFile = "loss_result.csv"
 
 filenames = ["slurm-1252331.out","slurm-1285335.out","slurm-1315260.out","slurm-1349088.out","slurm-1394929.out",
-"slurm-1437179.out","slurm-1469937.out","slurm-1490872.out","slurm-1519722.out","slurm-1541837.out"]
+"slurm-1437179.out","slurm-1469937.out","slurm-1490872.out","slurm-1519722.out","slurm-1541837.out","slurm-1584280.out","slurm-1638910.out",
+"slurm-1669724.out","slurm-1711503.out","slurm-1748175.out","slurm-1773681.out","slurm-1807344.out","slurm-1833218.out","slurm-1851426.out","slurm-1966856.out"]
 
 filetext = ""
 for i in filenames:
