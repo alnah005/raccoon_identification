d3eaa7546bb5583129cf55e661c3782f21099116
Suhail <alnah005@umn.edu>
2021-04-05 16:14:00 -0500

    added job scripts and removed fashionmnist from repo

---
+++ Triplet_Loss_Framework/commands.sh
@@ -0,0 +1,3 @@
+conda activate nvidia
+python -m pip install faiss-gpu
+python ~/raccoon_identification/Triplet_Loss_Framework/metric_learning.py
---
+++ Triplet_Loss_Framework/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=00:59:00
+#SBATCH --mail-type=ALL
+#SBATCH --mem=32GB
+#SBATCH --job-name=triplet_loss_raccoons
+#SBATCH --mail-user=alnah005@umn.edu
+#SBATCH -p v100                                            
+#SBATCH --gres=gpu:v100:1
+
+cd $SLURM_SUBMIT_DIR
+module load singularity
+pwd
+singularity exec --nv -i nv_od_local_v1.sif  raccoon_identification/Triplet_Loss_Framework/commands.sh
